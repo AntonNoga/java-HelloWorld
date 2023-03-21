da53aaede2ec22e522f83525fab8c29cbd943373
Anton Noga <noga.anton@pdffiller.team>
2023-03-21 16:37:19 +0200

    changes in branch after pull request

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -3,7 +3,6 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        //some changes
-        //some changes in branch after pull request
+        //changes into main branch
     }
 }
